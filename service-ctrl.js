ca54a59e31fd2a3ff7fdee0f4b3b238ff9a61bf0
Bhargavi-Kodidela <bhargavi@vivasa.in>
2016-09-23 16:04:55 +0530

    files modified

--- service-ctrl.js
+++ service-ctrl.js
@@ -8,26 +8,14 @@ angular.module("RDash").controller("TestController",["$scope", "MF3HTTPService",
 		function listOfServices(beanUrl){
 			MF3HTTPService.get(beanUrl,{ }).then(function(response){
 				$scope.contents = response.data;
-				//$scope.name = "john";
-
+				
 				console.log("Hurray!! Service returned with data: "+response);
 				
 				console.log(beanUrl);
 				console.log(response);
 
 			});
-			
-
-			/*console.log("Line 2");
-
-			MF3HTTPService.get(beanUrl,{}).then(function(data){
-				console.log("line 3 ");
-			});
-			console.log("Line 4");	*/	
 		}
-
-
-
 	};
 
 
